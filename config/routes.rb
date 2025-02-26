d3d4e6e93077ec3d69649a3c4de3d13f67a36543
Jake Erickson <jake212e@gmail.com>
2025-02-26 04:37:16 +0000

    Entry Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  get("/", { :controller => "places", :action => "index" })
+
   get("/places", { :controller => "places", :action => "index" })
+  get("/places/new", { :controller => "places", :action => "new" })
+  post("/places", { :controller => "places", :action => "create" })
+  get("/places/:id", { :controller => "places", :action => "show" })
 
-
+  get("/places/:place_id/entries/new", { :controller => "entries", :action => "new" })
+  post("/places/:place_id/entries", { :controller => "entries", :action => "create" })
 end
